53da725420bf33860f0d49f766fe6af107ba43b1
Mramington <codemuniqus@mail.ru>
2023-01-14 21:34:44 +0300

    Немного изменений

--- Hackathon.py
+++ Hackathon.py
@@ -1,10 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QInputDialog, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, \
-    QLineEdit
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication, QInputDialog, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QLineEdit
 from PyQt5.QtCore import Qt
 
 
-class Challenges(QMainWindow):
+class Challenges(QWidget):
     def __init__(self):
         super().__init__()
 
@@ -20,6 +19,8 @@ class Challenges(QMainWindow):
         self.hbox3 = QHBoxLayout()
         self.label_active_tasks = QLabel('Active Tasks', self)  # area for active tasks
         self.label_finished_tasks = QLabel('Finished Tasks', self)  # area for completed tasks
+        self.active_tasks = QLineEdit()
+        self.finished_tasks = QLineEdit()
 
         self.initUI()
 
@@ -28,7 +29,16 @@ class Challenges(QMainWindow):
         self.show()
 
     def set_structure(self):
+        self.hbox1.addWidget(self.label_active_tasks)
+        self.hbox1.addWidget(self.label_finished_tasks)
+        self.vbox.addLayout(self.hbox1)
+
+        self.hbox2.addWidget(self.active_tasks)
+        self.hbox2.addWidget(self.finished_tasks)
+        self.vbox.addLayout(self.hbox2)
+
         self.create_buttons()  # buttons on window
+
         self.setLayout(self.vbox)
 
     def create_buttons(self):
@@ -36,14 +46,14 @@ class Challenges(QMainWindow):
         button_finish_task = QPushButton('Finish Task', self)  # button "Finish Task"
         button_del_task = QPushButton('Delete Task', self)  # button "Delete Task"
 
-        self.hbox1.addWidget(button_add_task)
+        self.hbox3.addWidget(button_add_task)
         # self.vbox.addLayout(self.hbox1)
 
-        self.hbox1.addWidget(button_finish_task)
+        self.hbox3.addWidget(button_finish_task)
         # self.vbox.addLayout(self.hbox2)
 
-        self.hbox1.addWidget(button_del_task)
-        self.vbox.addLayout(self.hbox1)
+        self.hbox3.addWidget(button_del_task)
+        self.vbox.addLayout(self.hbox3)
 
 
 if __name__ == '__main__':
